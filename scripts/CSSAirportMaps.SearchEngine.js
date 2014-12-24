268a1555293e6ad51a6080d4912f60851acdbe3f
cssetian <chris.setian@gmail.com>
2014-12-24 12:56:08 -0500

    removed closure from search engine

--- scripts/CSSAirportMaps.SearchEngine.js
+++ scripts/CSSAirportMaps.SearchEngine.js
@@ -1,66 +1,59 @@
-(function(){
-  CSSAirportMaps.SearchEngine = function(options) {
-    // Initialize the Bloodhound search engine
-    console.log('SearchEngine.body: Initializing Bloodhound engine');
-    var self = this;
+CSSAirportMaps.SearchEngine = function(options) {
+  // Initialize the Bloodhound search engine
+  console.log('SearchEngine.body: Initializing Bloodhound engine');
+  var self = this;
 
-    self.mapSearchResults = function(bloodhoundSearchResultObj) {
+  self.mapSearchResults = function(bloodhoundSearchResultObj) {
 
-      // Map the properties of the returned geoplanet data to the viewmodel properties
-      var mappedOutputPreFiltering = $.map(bloodhoundSearchResultObj.geonames, function (airportSearchResultItem) {
+    // Map the properties of the returned geoplanet data to the viewmodel properties
+    var mappedOutputPreFiltering = $.map(bloodhoundSearchResultObj.geonames, function (airportSearchResultItem) {
 
-        // Attempt to match the name of one of the alternateNames in a geoData result item to the official airport code name (IATA)
-        var _IATACode = _.filter(airportSearchResultItem.alternateNames, function(alternateNamesItem) {
-          return alternateNamesItem.lang === 'iata';
-        });
-        var _filteredIATACode = '';
-        if(typeof _IATACode !== 'undefined' && _IATACode.length > 0) {
-          _filteredIATACode = _IATACode[0].name;
-        }
-
-        // Return a new json object where only the relevant properties are kept and mapped
-        return {
-          value: airportSearchResultItem.toponymName,
-          name: airportSearchResultItem.toponymName,
-          lat: airportSearchResultItem.lat,
-          lng: airportSearchResultItem.lng,
-          city: airportSearchResultItem.adminName2,
-          state: airportSearchResultItem.adminName1,
-          country: airportSearchResultItem.countryName,
-          countryCode: airportSearchResultItem.countryCode,
-          adminId1: airportSearchResultItem.adminId1,
-          geoNameId: airportSearchResultItem.geonameId,
-          timeZone: airportSearchResultItem.timezone,
-          code: _filteredIATACode,
-          id: options.id
-        };
+      // Attempt to match the name of one of the alternateNames in a geoData result item to the official airport code name (IATA)
+      var _IATACode = _.filter(airportSearchResultItem.alternateNames, function(alternateNamesItem) {
+        return alternateNamesItem.lang === 'iata';
       });
+      var _filteredIATACode = '';
+      if(typeof _IATACode !== 'undefined' && _IATACode.length > 0) {
+        _filteredIATACode = _IATACode[0].name;
+      }
 
-      // Filter out airports lacking IATA codes - they're probably not legit
-      var mappedOutput = _.filter(mappedOutputPreFiltering, function(item) {
-        return (typeof item.code !== 'undefined' && item.code !== null && item.code !== '');
-      });
-      return mappedOutput;
-    };
+      // Return a new json object where only the relevant properties are kept and mapped
+      return {
+        value: airportSearchResultItem.toponymName,
+        name: airportSearchResultItem.toponymName,
+        lat: airportSearchResultItem.lat,
+        lng: airportSearchResultItem.lng,
+        city: airportSearchResultItem.adminName2,
+        state: airportSearchResultItem.adminName1,
+        country: airportSearchResultItem.countryName,
+        countryCode: airportSearchResultItem.countryCode,
+        adminId1: airportSearchResultItem.adminId1,
+        geoNameId: airportSearchResultItem.geonameId,
+        timeZone: airportSearchResultItem.timezone,
+        code: _filteredIATACode,
+        id: options.id
+      };
+    });
 
-    
-    self.filterAndMapSearchResults = function(bloodhoundSearchResultObj) {
-      console.log('************************* COMPLETED BLOODHOUND AIRPORT SEARCH EXECUTION ****************************');
-      console.log('SearchEngine.remoteFilter: (Bloodhound Callback) Found ' + bloodhoundSearchResultObj.length + ' airport(s)! (Obj copied below) ---v');
-      console.log(bloodhoundSearchResultObj);
-
-      console.log('SearchEngine.remoteFilter: Mapping results!');
-      var mappedSearchResults = self.mapSearchResults(bloodhoundSearchResultObj);
-
-      console.log('SearchEngine.remoteFilter: (Bloodhound Callback) Mapped Output (Obj copied below) --v');
-      console.log(mappedSearchResults);
-      return mappedSearchResults;
-    };
-
-
-  return {  remoteFilter: self.filterAndMapSearchResults,
-            search: self.search,
-            mapSearchResults: self.mapSearchResults
-          };
+    // Filter out airports lacking IATA codes - they're probably not legit
+    var mappedOutput = _.filter(mappedOutputPreFiltering, function(item) {
+      return (typeof item.code !== 'undefined' && item.code !== null && item.code !== '');
+    });
+    return mappedOutput;
   };
-})();
+
+  
+  self.remoteFilter = function(bloodhoundSearchResultObj) {
+    console.log('************************* COMPLETED BLOODHOUND AIRPORT SEARCH EXECUTION ****************************');
+    console.log('SearchEngine.remoteFilter: (Bloodhound Callback) Found ' + bloodhoundSearchResultObj.length + ' airport(s)! (Obj copied below) ---v');
+    console.log(bloodhoundSearchResultObj);
+
+    console.log('SearchEngine.remoteFilter: Mapping results!');
+    var mappedSearchResults = self.mapSearchResults(bloodhoundSearchResultObj);
+
+    console.log('SearchEngine.remoteFilter: (Bloodhound Callback) Mapped Output (Obj copied below) --v');
+    console.log(mappedSearchResults);
+    return mappedSearchResults;
+  };
+
+};
